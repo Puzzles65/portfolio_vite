25a36b9a992d659747d31ec53a53fc33e12b7d00
Puzzles65 <chitrungnguyen12@gmail.com>
2024-03-03 19:22:07 +0000

    gradient makes navbar unusable

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,9 +17,9 @@ function Home() {
       <div className={`xl:px-0 px-6`}>
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[80px] leading-[75px]">
           Hello! <br className="sm:block hidden" /> {/* gradient start */}
-          <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+          {/* <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
           <div className="absolute z-[1] w-[10%] h-[80%] rounded-full white__gradient bottom-40" />
-          <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+          <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" /> */}
           {/* gradient end */}
         </h1>
         <h2 className="text-gradient ss:text-[45px] text-[30px]">
