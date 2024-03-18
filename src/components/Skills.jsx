a0c1a7d78ab5669dfcfd3a6f7e5d0c97ac3695c7
Puzzles65 <chitrungnguyen12@gmail.com>
2024-03-18 17:22:11 +0000

    some styling to skills, need to make them into cards

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,57 +1,65 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faJs, faPython, faHtml5 } from "@fortawesome/free-brands-svg-icons";
-import { DiPostgresql } from "react-icons/di";
-import { IoLogoCss3 } from "react-icons/io";
-import { SiMongodb } from "react-icons/si";
-import { IoLogoSass } from "react-icons/io5";
-import { DiDjango } from "react-icons/di";
-import { SiExpress } from "react-icons/si";
-import { SiMongoose } from "react-icons/si";
-import { FaBootstrap } from "react-icons/fa6";
-import { FaChrome } from "react-icons/fa";
-import { DiVisualstudio } from "react-icons/di";
-import { SiJira } from "react-icons/si";
-import { SiPostman } from "react-icons/si";
-import { FaGithub } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
+import React from "react";
+import {
+  FaPython,
+  IoLogoJavascript,
+  FaHtml5,
+  DiPostgresql,
+  IoLogoCss3,
+  SiExpress,
+  SiMongodb,
+  IoLogoSass,
+  DiDjango,
+  SiJira,
+  SiMongoose,
+  FaBootstrap,
+  FaChrome,
+  DiVisualstudio,
+  SiPostman,
+  FaGithub,
+  SiTailwindcss,
+} from "./SkillsIcons";
 
-
-
-
-
+const SkillItem = ({ Icon, text }) => (
+  <div className="flex flex-row items-center">
+    <Icon className="mr-2 mt-2 mb-2 text-5xl" />
+    <p className="text-xl">{text}</p>
+  </div>
+);
 
 const Skills = () => {
   return (
-    <section id="skills">
-      <div className="text-white">
+    <section id="skills" className="flex flex-col">
       <h1 className="text-gradient text-2xl font-bold">SKILLS</h1>
-      <h2>Programming Languages and Databases</h2>
-      <FontAwesomeIcon icon={faJs} />
-      <FontAwesomeIcon icon={faPython} />
-      <FontAwesomeIcon icon={faHtml5} />
-     <DiPostgresql />
-      <IoLogoCss3 />
-      <SiMongodb />
-      <IoLogoSass />
+
+      <div className="sm:flex flex-row justify-evenly">
+        <div className="text-white text-5xl">
+          <h2 className="text-2xl text-gradient">Programming Languages &</h2>
+          <h2 className="text-2xl text-gradient text-center">Databases</h2>
+          <SkillItem Icon={FaPython} text="Python" />
+          <SkillItem Icon={IoLogoJavascript} text="JavaScript" />
+          <SkillItem Icon={FaHtml5} text="HTML5" />
+          <SkillItem Icon={DiPostgresql} text="PostgreSQL" />
+          <SkillItem Icon={IoLogoCss3} text="CSS3" />
+          <SkillItem Icon={SiMongodb} text="MongoDB" />
+          <SkillItem Icon={IoLogoSass} text="Sass" />
+        </div>
+        <div className="text-white text-5xl">
+          <h2 className="text-2xl text-gradient text-center">Frameworks</h2>
+          <SkillItem Icon={DiDjango} text="Django" />
+          <SkillItem Icon={SiExpress} text="Express.js" />
+          <SkillItem Icon={SiMongoose} text="Mongoose" />
+          <SkillItem Icon={FaBootstrap} text="Bootstrap" />
+        </div>
+        <div className="text-white text-5xl">
+          <h2 className="text-2xl text-gradient text-center">Tools</h2>
+          <SkillItem Icon={FaChrome} text="Chrome Dev Tools" />
+          <SkillItem Icon={DiVisualstudio} text="Visual Studio Code" />
+          <SkillItem Icon={SiJira} text="Jira" />
+          <SkillItem Icon={SiPostman} text="Postman" />
+          <SkillItem Icon={FaGithub} text="GitHub" />
+          <SkillItem Icon={SiTailwindcss} text="Tailwind CSS" />
+        </div>
       </div>
-      <div className="text-white">
-      <h2>Frameworks</h2>
-      <DiDjango />
-      <SiExpress />
-      <SiMongoose />
-      <FaBootstrap />
-       </div>
-       <div className="text-white">
-       <h2>Tools</h2>
-       <FaChrome />
-       <DiVisualstudio />
-       <SiJira />
-       <SiPostman />
-       <FaGithub />
-       <SiTailwindcss />
-
-       </div>
-
     </section>
   );
 };
---
+++ src/components/SkillsIcons.js
@@ -0,0 +1,38 @@
+import { FaPython } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaHtml5 } from "react-icons/fa";
+
+import { DiPostgresql } from "react-icons/di";
+import { IoLogoCss3 } from "react-icons/io";
+import { SiMongodb } from "react-icons/si";
+import { IoLogoSass } from "react-icons/io5";
+import { DiDjango } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { SiMongoose } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa6";
+import { FaChrome } from "react-icons/fa";
+import { DiVisualstudio } from "react-icons/di";
+import { SiJira } from "react-icons/si";
+import { SiPostman } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+
+export {
+  FaPython,
+  IoLogoJavascript,
+  FaHtml5,
+  DiPostgresql,
+  IoLogoCss3,
+  SiExpress,
+  SiMongodb,
+  IoLogoSass,
+  DiDjango,
+  SiJira,
+  SiMongoose,
+  FaBootstrap,
+  FaChrome,
+  DiVisualstudio,
+  SiPostman,
+  FaGithub,
+  SiTailwindcss,
+};
